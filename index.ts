515bcc700d89bd4915bfe19dd87e20635ee03457
MarcuAdrian18 <80761236+MarcuAdrian18@users.noreply.github.com>
2022-09-07 16:59:29 +0300

    Refactoring

--- index.ts
+++ index.ts
@@ -1,7 +1,23 @@
 import axios from "axios";
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
 axios.get(url).then((response) => {
-  console.log(response.data);
+  const todo = response.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const finished = todo.completed;
+
+  console.log(`
+    The TODO with ID: ${id} 
+    Has a title of: ${title}
+    Is it finished? ${finished}
+  `);
 });
